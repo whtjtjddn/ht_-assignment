fb75e4b0f07475dcf47390d6866b995d26973e7f
whtjtjddn <whtjtjddn@naver.com>
2025-05-07 21:32:11 +0900

    Set firebase deploy

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import * as functions from "firebase-server";
+
+// // Start writing server
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = server.https.onRequest((request, response) => {
+//   server.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
