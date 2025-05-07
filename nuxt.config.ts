55b88250521b50abef37b87b372eea32de4c3a89
whtjtjddn <whtjtjddn@naver.com>
2025-05-07 22:06:07 +0900

    Set firebase deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
         firebase: {
             nodeVersion: "18",
             gen: 2,
-            serverFunctionName: "server",
+            serverFunctionName: "functions",
             httpsOptions: {
                 region: "asia-northeast3",
                 memory: "1GiB"
