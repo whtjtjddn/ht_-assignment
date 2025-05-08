c4e2f896d9f88a16459af7643058abc17f1c5460
whtjtjddn <whtjtjddn@naver.com>
2025-05-08 19:43:04 +0900

    Add Open AI feature

--- composables/useOpenai.ts
+++ composables/useOpenai.ts
@@ -12,7 +12,7 @@ export default function () {
 
         const response = await client.responses.create({
             model: "gpt-4.1",
-            input: `${region}의 ${station}역의 정보와 놀거리, 맛집 정보들을 예쁜 마크다운 형식으로 내려줘. 서두랑 마무리 멘트는 필요없고, 그냥 딱 마크다운 viewer에서 사용가능하도록 정보만 내려줘. 앞의 '₩₩₩markdown 요런것도 자르구'`
+            input: `${region}의 ${station}역의 정보와 놀거리, 맛집 정보들을 예쁜 모바일 사이즈 마크다운 형식으로 내려줘. 서두랑 마무리 멘트는 필요없고, 그냥 딱 마크다운 viewer에서 사용가능하도록 정보만 내려줘. 앞의 '₩₩₩markdown 요런것도 자르구'`
         })
 
         console.log(response.output_text)
