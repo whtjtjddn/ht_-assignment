8e50d52b57f9d87fe8855eacc03381e2d63592c2
whtjtjddn <whtjtjddn@naver.com>
2025-05-13 17:45:30 +0900

    Change to default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,7 +57,8 @@ export default defineNuxtConfig({
                     rel: "icon",
                     type: "image/x-icon",
                     href: "/favicon.ico"
-                }
+                },
+                { rel: "canonical", href: "https://subway-roulette.store" }
             ],
             meta: [
                 {
@@ -79,6 +80,12 @@ export default defineNuxtConfig({
                 {
                     name: "og:description",
                     content: "세상의 모든 P들 하고싶은건 많지만 계획짜긴 귀찮을때, 지하철 랜덤 여행을 같이 떠나보아요"
+                }
+            ],
+            script: [
+                {
+                    src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9206452158611873",
+                    crossorigin: "anonymous"
                 }
             ]
         }
